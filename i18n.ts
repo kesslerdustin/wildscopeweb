a77c6a3119ca956288703393808edc2e54184796
kesslerdustin <dustin.kessler@immotech.io>
2025-05-08 16:37:35 +0200

    test3

--- i18n.ts
+++ i18n.ts
@@ -7,6 +7,7 @@ export default getRequestConfig(async ({requestLocale}) => {
   const locale = await requestLocale || defaultLocale;
   
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   };
 });
